5d8e56826d150957d0ea47ea3705a505b00336ff
Kirsi Mak <ying987554@hotmail.com>
2023-11-23 01:23:32 +0800

    Update init.js

--- main/models/init.js
+++ main/models/init.js
@@ -16,6 +16,7 @@ mongoose.set("strictQuery", true)
 mongoose.connect(url)
 console.log(mongoose.connection.readyState);
 
+//User
 const deleteAndCreateUsers = async () => {
     try {
       await User.deleteMany({});
@@ -29,4 +30,86 @@ const deleteAndCreateUsers = async () => {
     }
 }
 
-deleteAndCreateUsers();
+//Dept
+const deleteAndCreateDepts = async () => {
+    try {
+      await Department.deleteMany({});
+      console.log("Del dept");
+      const depts = JSON.parse(fs.readFileSync("./main/sampleData/SE_Project.departments.json", "utf-8"));
+      await Department.insertMany(depts);
+      console.log('New dept created');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+//Course
+const deleteAndCreateCourses = async () => {
+    try {
+      await Course.deleteMany({});
+      console.log("Del course");
+      const courses = JSON.parse(fs.readFileSync("./main/sampleData/SE_Project.courses.json", "utf-8"));
+      await Course.insertMany(courses);
+      console.log('New course created');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+//Program
+const deleteAndCreatePrograms = async () => {
+    try {
+      await Program.deleteMany({});
+      console.log("Del prog");
+      const courses = JSON.parse(fs.readFileSync("./main/sampleData/SE_Project.programs.json", "utf-8"));
+      await Program.insertMany(courses);
+      console.log('New prog created');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+//CourseDetails
+const deleteAndCreateCourseDetails = async () => {
+    try {
+      await CourseDetail.deleteMany({});
+      console.log("Del courseDetail");
+      const courDet = JSON.parse(fs.readFileSync("./main/sampleData/SE_Project.courseDetails.json", "utf-8"));
+      await CourseDetail.insertMany(courDet);
+      console.log('New courseDetail created');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+//CourseStudents
+const deleteAndCreateCourseStudents = async () => {
+    try {
+      await CourseStudent.deleteMany({});
+      console.log("Del courseStudent");
+      const courStud = JSON.parse(fs.readFileSync("./main/sampleData/SE_Project.courseStudents.json", "utf-8"));
+      await CourseStudent.insertMany(courStud);
+      console.log('New courseStudent created');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+}
+
+
+const setupData = async () => {
+    try {
+        deleteAndCreateUsers();
+        deleteAndCreateDepts();
+        deleteAndCreateCourses();
+        deleteAndCreatePrograms();
+        deleteAndCreateCourseDetails();
+        deleteAndCreateCourseStudents();
+    } catch (error) {
+      console.error('Error:', error);
+    }finally{
+        console.log('finish');
+    }
+  }
+  
+  setupData();
+
