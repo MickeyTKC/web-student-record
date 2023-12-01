68aaffa5b3e65cd5a43a3a332b9f47164493e452
MickeyTKC <tt51145505@gmail.com>
2023-12-01 13:52:12 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,52 +1,91 @@
 const mongoose = require("mongoose");
 
+const Course = require("./Course");
+const CourseDetail = require("./CourseDetail");
+const Program = require("./Program");
+const Department = require("./Department");
+
+const getCourse = {
+  student: () => {
+    return "role:student ()=> getCourse"
+  },
+  teacher: () => {
+    return "role:teacher ()=> getCourse"
+  },
+  admin: () => {
+    return "role:admin ()=> getCourse"
+  },
+};
+
+const getCourseFactory = role => {
+  if (role == "admin") return getCourse.admin;
+  if (role == "teacher") return getCourse.teacher;
+  return getCourse.student;
+};
+
 const userSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["student", "teacher", "admin"], required: true },
+  role: {
+    type: String,
+    enum: ["student", "teacher", "admin"],
+    required: true,
+  },
   name: { type: String },
   department: { type: String },
-  major: { type: String },  //if teacher just ""
-	progYear: { type: String }, //e.g. "year3 entry", if teacher ""
+  major: { type: String }, //if teacher just ""
+  progYear: { type: String }, //e.g. "year3 entry", if teacher ""
   dateFrom: { type: String }, //date of join HKMU
   dateTo: { type: String }, //date of expected graduate, if teacher just ""
-  email: { type: String }, 
+  email: { type: String },
   phoneNo: { type: String },
-  emergencyPhoneNo: { type: String }
+  emergencyPhoneNo: { type: String },
 });
 
 //------------------Function start------------------
 
-userSchema.statics.findByUserId = function (userId){  //find by id
-    return this.findOne({id: userId});
-}
+userSchema.statics.findByUserId = function (userId) {
+  //find by id
+  return this.findOne({ id: userId });
+};
 
-userSchema.statics.findByRole = function (role){  //find by role
-    return this.find({role: role});
-}
+userSchema.statics.findByRole = function (role) {
+  //find by role
+  return this.find({ role: role });
+};
 
-userSchema.statics.findByName = function (name){  //find by name
-    return this.find({name: name});
-}
+userSchema.statics.findByName = function (name) {
+  //find by name
+  return this.find({ name: name });
+};
 
-userSchema.statics.findByDept = function (dept){  //find by department
-    return this.find({department: dept});
-}
+userSchema.statics.findByDept = function (dept) {
+  //find by department
+  return this.find({ department: dept });
+};
 
-userSchema.statics.findByMajor = function (major){  //find by major
-    return this.find({major: major});
-}
+userSchema.statics.findByMajor = function (major) {
+  //find by major
+  return this.find({ major: major });
+};
 
-userSchema.statics.findBydateFrom = function (date){  //find by date start
-    return this.find({dateFrom: date});
-}
+userSchema.statics.findBydateFrom = function (date) {
+  //find by date start
+  return this.find({ dateFrom: date });
+};
 
-userSchema.statics.findBydateTo = function (date){  //find by date expected graduate
-    return this.find({dateTo: date});
-}
+userSchema.statics.findBydateTo = function (date) {
+  //find by date expected graduate
+  return this.find({ dateTo: date });
+};
 
-userSchema.statics.findByEmail = function (email){  //find by email
-    return this.find({email: email});
-}
+userSchema.statics.findByEmail = function (email) {
+  //find by email
+  return this.find({ email: email });
+};
+
+userSchema.methods.getRole = function () {
+  return {getCoutse:getCourseFactory(this.role)};
+};
 
 module.exports = mongoose.model("User", userSchema);
