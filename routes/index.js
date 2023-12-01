8f2e149e54413069db859305f9b2e5d462398177
MickeyTKC <tt51145505@gmail.com>
2023-12-01 13:52:22 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+const authRoute = require("./auth");
 
-router.get("/*",(req,res,next)=>{
-    console.log("api does not exsit.")
-})
+router.get("/", (req, res, next) => {
+  return next({ statusCode: 404, message: "API is Not Found" });
+});
+
+router.use("/auth", authRoute);
+
+router.get("/*", (req, res, next) => {
+  return next({ statusCode: 404, message: "API is Not Found" });
+});
+
+router.use((err, req, res, next) => {
+  if (!err) {
+    err.statusCode = 500;
+    err.message = "Internal Server Error";
+  }
+  res.status(err.statusCode).json({ message: err.message });
+});
 
 module.exports = router;
