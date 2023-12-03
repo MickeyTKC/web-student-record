7a5d77dfc201c3747b00b705bcba82b328771427
MickeyTKC <tt51145505@gmail.com>
2023-12-03 18:01:09 +0800

    Create department.js

---
+++ routes/department.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+
+const Department = require("../models/Department");
+
+router.post("/departments", async (req, res) => {
+  try {
+    const department = new Department(req.body);
+    await department.save();
+    res.status(201).json(department);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/departments", async (req, res) => {
+  try {
+    const departments = await Department.find();
+    res.json(departments);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/departments/:id", async (req, res) => {
+  try {
+    const department = await Department.findById(req.params.id);
+    if (!department) {
+      return res.status(404).json({ message: "Department not found" });
+    }
+    res.json(department);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.patch("/departments/:id", async (req, res) => {
+  try {
+    const department = await Department.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!department) {
+      return res.status(404).json({ message: "Department not found" });
+    }
+    res.json(department);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete("/departments/:id", async (req, res) => {
+  try {
+    const department = await Department.findByIdAndDelete(req.params.id);
+    if (!department) {
+      return res.status(404).json({ message: "Department not found" });
+    }
+    res.json({ message: "Department deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
